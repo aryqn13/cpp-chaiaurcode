00c4e1a7d67a43c90014b254eec197418e883af4
Aryan Rajput <aryangrajput13@gmail.com>
2024-10-04 15:55:02 +0530

    dataTypes.cpp added which has intro to basic data types

---
+++ primitives/dataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+// #include<bits/stdc++.h>
+#include<string>
+
+using namespace std;
+
+//log a film in letterboxd journal
+int main(){
+
+    unsigned int filmYear = 1957;
+    float timeDuration = 146.27;
+    double yourRating = 4.77;
+    char filmTier = 'A';
+    string filmName = "Pyaasa";
+    string yourReview;
+    
+    cout<<filmName<<" "<<(filmYear);
+    cin>> yourReview;
+
+    return 0;
+}
